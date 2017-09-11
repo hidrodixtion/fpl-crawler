abee725baaf8143d33aa34ae4c0c86be5170dfc9
Adi Nugroho <hidrodixtion@gmail.com>
2017-09-11 20:33:38 +0700

    fix getting all points

--- fpl_indo_crawl.rb
+++ fpl_indo_crawl.rb
@@ -44,49 +44,33 @@ def scrape_ids
 end
 
 def scrape_points
-  id = @ids[0]
+  @ids.each do |id|
+    get_point id 
+  end
+end
 
-  # ids.each do |id|
+def get_point(id, browser = nil)
   url = "https://fantasy.premierleague.com#{id[:url]}"
-  browser = Watir::Browser.new(:chrome, headless: false)
+  
+  if browser == nil
+    browser = Watir::Browser.new(:chrome, headless: true)
+  end
+  
   browser.goto url # "https://fantasy.premierleague.com/#{id[:url]}/event/4"
   doc = Nokogiri::HTML.parse(browser.html)
 
-  point_path = doc.css("div.ism-scoreboard-points")
-  p doc.css("ism-scoreboard__panel__value")
-  # p doc.css("div.ism-scoreboard-points")
-  # p doc
-  # id[:gw_point] = point_path.text.strip
-# end
+  point_path = doc.css("div.ism-scoreboard__panel__value.ism-scoreboard__panel__value--lrg")
+
+  id[:gw_point] = point_path.text.strip.split("\n")[0]
+  if id[:gw_point] == nil
+    get_point(id, browser)
+  else
+    p id
+
+    browser.close
+  end
 end
 
 init()
-
-
-
-# p ids
-# uncomment 2 lines below to limit array (for testing purpose)
-# set = *(2..ids.length)
-# ids.delete_if.with_index {|_, index| set.include? index}
-
-# get latest point
-# ids.each do |id|
-#   doc = Nokogiri::HTML(open("http://id.fantasy.premierleague.com#{id[:url]}"))
-#   doc.xpath("//div[contains(@class, 'ism-scoreboard-panel__points')]").collect do |node|
-#     id[:gw_point] = node.text.strip.gsub(/[poin]/, '').to_i
-
-#     # won't work if score already updated / will be updated
-#     id[:total_point] = id[:last_point] + id[:gw_point]# - id[:not_updated_gw_point]
-#   end
-# end
-
-# # print only latest score (& sort it)
-# ids.sort_by! {|hash| hash[:gw_point]}
-# ids.reverse!
-# ids.each {|hash| p "#{hash[:name]} : #{hash[:gw_point]}"}
-# p "------"
-# ids.sort_by! {|hash| hash[:total_point]}
-# ids.reverse!
-# ids.each {|hash| p "#{hash[:name]} : #{hash[:total_point]}"}
 
 #p Time.now.strftime("%H : %M")
